9697dfb8dfdec9ba7a64665ccb1d7a4d6095450f
Nilesh Tyagi <109848404+NileshTyagi@users.noreply.github.com>
2025-03-09 00:53:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base:"/fluid_cursor/"
+// })
 export default defineConfig({
+  base: './',
   plugins: [react()],
-  base:"/fluid_cursor/"
-})
+});
